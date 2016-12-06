9e1fab421e409cb473ee3e1549fbacadfc71b08b
Barco <barcozaleta@gmail.com>
2016-12-06 14:20:31 +1300

    Initial commit

---
+++ android/src/com/urgentx/sen/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.urgentx.sen;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.urgentx.sen.Sen;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Sen(), config);
+	}
+}
---
+++ desktop/src/com/urgentx/sen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.urgentx.sen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.urgentx.sen.Sen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sen(), config);
+	}
+}
